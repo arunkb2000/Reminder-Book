abb184885c658b19ef4cf805d617fa4ded701ca0
arunkb2000 <akb15@iitbbs.ac.in>
2021-05-14 16:49:36 +0530

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,122 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class ="container">
+    <Header @toggle-add-task = "toggleAddTask"
+     title=" Reminder Book"
+     :showAddTask="showAddTask" />
+    <div v-show = "showAddTask">
+    <AddTask @add-task="addTask"/>
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" 
+    @delete-task ="deleteTask" :tasks="tasks"/> 
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//to import the header file.
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data(){
+    return{
+      tasks: [],
+      showAddTask : false,
+    }
+  },
+  
+  methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id) {
+      if(confirm('Are you sure?')){
+    this.tasks = this.tasks.filter((task) => task.id !==id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task) =>
+      task.id ===id ? { ...task, reminder: !task.reminder} :task
+      )
+    },
+  },
+  created(){
+    this.tasks = [
+    {
+         id: 1,
+         text: 'Doctors Appointment',
+         day: 'May 21st at 2:30pm',
+         reminder: true,
+     },
+
+    {
+         id: 2,
+         text: 'School Meeting',
+         day: 'May 23rd at 2:00pm',
+         reminder: false,
+     },
+     {
+         id: 3,
+         text: 'college Meeting',
+         day: 'May 30th at 10:00am',
+         reminder: true,
+     },
+
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1> {{title}}</h1>
+        <Button  @btn-click ="$emit('toggle-add-task')" 
+        :text ="showAddTask ? 'Close' : 'Add'"
+        :color ="showAddTask ? 'red' : 'green'"/>
+       
+        
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    //props: ['title'],
+    //another way we can do this 
+    props: {
+      title: String,
+    showAddTask: Boolean,
+    },
+   //  default: " This is a earlier message."  
+   // default message will display when there where nothing is defined in App.vue files component Header.
+   // }
+   components: {
+       Button,
+   },
+}
+</script>
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
